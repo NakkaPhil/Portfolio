1293a68ec05df895019a7ef7d3de946dcf17e68f
NakkaPhil <philbusiness360@gmail.com>
2022-08-11 19:59:00 -0400

    Commit del 11/08

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,7 +1,16 @@
-function desplegarMenu(){
-    const boton = document.querySelector('#menu-mobile')
 
-    boton.addEventListener('click', () =>{
-        boton.classList.add('mostrar')
-    })
-}
+
+const boton = document.querySelector('#menu-mobile')
+boton.addEventListener('click',  ()=>{
+
+    const bloque_nav = document.querySelector('.bloque1-nav')
+    bloque_nav.classList.add('mostrar') 
+
+    //Creamos el boton de cerrar:
+    const cerrarBtn = document.createElement('IMG');
+    cerrarBtn.classList.add('boton-cerrar')
+    cerrarBtn.src = './assets/img/close.png';
+    
+    //Lo añadimos al bloque de menú:
+    bloque_nav.appendChild(cerrarBtn);
+})
