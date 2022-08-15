13a8d59aee7d212823430fb0d37f9834ee318069
Philippe <sevasdoge.01@gmail.com>
2022-08-15 15:56:00 -0400

    Se ha añadido menu

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,16 +1,34 @@
 
-
+//Boton para abrir menu
 const boton = document.querySelector('#menu-mobile')
+
+
+//Creamos el boton de cerrar:
+const cerrarBtn = document.createElement('IMG');
+cerrarBtn.classList.add('boton-cerrar')
+cerrarBtn.src = './assets/img/close.png';
+
+const bloque_nav = document.querySelector('.bloque1-nav')
+
 boton.addEventListener('click',  ()=>{
 
-    const bloque_nav = document.querySelector('.bloque1-nav')
-    bloque_nav.classList.add('mostrar') 
+        bloque_nav.classList.add('mostrar') 
+           
+        //Lo añadimos al bloque de menú:
+        bloque_nav.appendChild(cerrarBtn);
+    })
 
-    //Creamos el boton de cerrar:
-    const cerrarBtn = document.createElement('IMG');
-    cerrarBtn.classList.add('boton-cerrar')
-    cerrarBtn.src = './assets/img/close.png';
     
-    //Lo añadimos al bloque de menú:
-    bloque_nav.appendChild(cerrarBtn);
-})
+if(cerrarBtn){
+    cerrarBtn.addEventListener('click', ()=> {
+        bloque_nav.classList.add('oculto')
+        setTimeout(() => {
+            bloque_nav.classList.remove('mostrar')
+            bloque_nav.classList.remove('oculto')
+        }, 750);
+    })
+}
+
+
+
+
